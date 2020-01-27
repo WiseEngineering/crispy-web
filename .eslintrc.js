e2fa1d65c1af1cb48f00a68cdde22acbd68339e3
svirgunvolodia <svirgun200@gmail.com>
2020-01-27 14:35:33 +0200

    init apollo & graphql codegen

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,6 @@ module.exports = {
   rules: {
     semi: ["error", "never"],
     '@typescript-eslint/indent': ["error", 2],
+    "react/prop-types": "off"
   }
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import { BrowserRouter } from 'react-router-dom'
 
 import PageWrapper from './components/PageWrapper'
 import Routes from './Routes'
+import { ApolloProvider } from './ApolloProvider'
 
 import './App.css'
 
 class App extends Component {
   render(): JSX.Element {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <PageWrapper>
-            <Routes />
-          </PageWrapper>
-        </div>
-      </BrowserRouter>
+      <ApolloProvider>
+        <BrowserRouter>
+          <div className="App">
+            <PageWrapper>
+              <Routes />
+            </PageWrapper>
+          </div>
+        </BrowserRouter>
+      </ApolloProvider>
     )
   }
 }
